952427fb9723e2d974775b6351f6c9c3c4781009
vjoe96 <87068212+vjoe96@users.noreply.github.com>
2022-04-18 19:34:47 +0900

    상세+트라이온

--- lippick/src/components/DetailProductPage.js
+++ lippick/src/components/DetailProductPage.js
@@ -1,58 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import DetailProductImage from "./DetailProductImage";
 import { Row, Col, Form, Button, Select, Image } from "antd";
-import { connect, useDispatch } from "react-redux"
+import { connect, useDispatch } from "react-redux";
 import { addToCart } from "../_actions/user_actions";
+import MakeUp from "./MakeUp";
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import Webcam from "react-webcam";
+import { MdTty } from "react-icons/md";
 
 function DetailProductPage(props) {
-
-
     useEffect(() => {
         props.dispatch({ type: "nav-on" });
-            
     }, []);
 
-
     const dispatch = useDispatch();
- 
+
     const productId = props.match.params.productId;
 
-    const [Product, setProduct] = useState({})
+    const [Product, setProduct] = useState({});
 
+    const [tryOn, setTryOn] = useState(false);
     useEffect(() => {
-            
-            axios.get(`/api/product/products_by_id?id=${productId}`)
-                .then(response => {
-                    console.log(response.data.success)
-                    if (response.data.success) {
-                        console.log('response.data', response.data)
-                        setProduct(response.data.product[0])
-                    } else {
-                        console.log('가져오기 실패')
-                    }
-
-                })
-
-        }, [])
+        axios
+            .get(`/api/product/products_by_id?id=${productId}`)
+            .then((response) => {
+                console.log(response.data.success);
+                if (response.data.success) {
+                    console.log("response.data", response.data);
+                    setProduct(response.data.product[0]);
+                } else {
+                    console.log("가져오기 실패");
+                }
+            });
+    }, []);
+    console.log(Product.color);
 
     const bagHandler = () => {
         // 필요한 정보를 cart field에 넣어준다.
-        dispatch(addToCart(productId))
-        
-
-    }
-
+        dispatch(addToCart(productId));
+    };
+    const changeTry = () => {
+        setTryOn(!tryOn);
+    };
 
     return (
-        <div style={{ width: '100%', padding: '3rem 4rem' }}>
-
+        <div style={{ width: "100%", padding: "3rem 4rem" }}>
             <br />
             <br />
 
-            <Row gutter={[30, 30]} >
+            <Row gutter={[30, 30]}>
                 <Col lg={12} sm={24}>
-                    <DetailProductImage detail={Product}/>
+                    {tryOn && <MakeUp color={Product.color} />}
+                    {!tryOn && <DetailProductImage detail={Product} />}
                 </Col>
                 <Col lg={12} sm={24}>
                     <br />
@@ -61,9 +61,9 @@ function DetailProductPage(props) {
                     <h3>{Product.title}</h3>
                     <hr />
                     <p>{Product.price} 원</p>
-                    <p style={{fontWeight : 'bold'}}>선택 가능한 컬러</p>
-                    <Select style={{ width: '200px' }}>
-                     <option>제품이름 같은 다른 놈</option>
+                    <p style={{ fontWeight: "bold" }}>선택 가능한 컬러</p>
+                    <Select style={{ width: "200px" }}>
+                        <option>제품이름 같은 다른 놈</option>
                     </Select>
                     <p> ^ 이곳에 제품이름 같은 놈들 링크 있으면 좋겠다</p>
                     <br />
@@ -79,7 +79,7 @@ function DetailProductPage(props) {
                     </Button>
                     <br />
                     <br />
-                    <Button size="large" shape="round">
+                    <Button size="large" shape="round" onClick={changeTry}>
                         트라이온
                     </Button>
                 </Col>
@@ -90,11 +90,11 @@ function DetailProductPage(props) {
             <hr />
             <p>상세 설명 이미지 | 근데 리뷰 기능 만들어 말어........</p>
             <Image
-                    src={`http://localhost:5000/${Product.description}`}
-                    preview={false}
-                />
-        </div> 
-    )
+                src={`http://localhost:5000/${Product.description}`}
+                preview={false}
+            />
+        </div>
+    );
 }
 
 function stateprops(state) {

2cc0dbe6fc1cb76a6d3d81b12ddb2bb4289dce44
youngwoo <lyw960820@icloud.com>
2022-04-19 22:28:00 +0900

    상세2

--- lippick/src/components/DetailProductPage.js
+++ lippick/src/components/DetailProductPage.js
@@ -8,108 +8,118 @@ import MakeUp from "./MakeUp";
 import * as facemesh from "@tensorflow-models/face-landmarks-detection";
 import Webcam from "react-webcam";
 import { MdTty } from "react-icons/md";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 function DetailProductPage(props) {
-    useEffect(() => {
-        props.dispatch({ type: "nav-on" });
-    }, []);
+  useEffect(() => {
+    props.dispatch({ type: "nav-on" });
+  }, []);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const productId = props.match.params.productId;
+  const productId = props.match.params.productId;
 
-    const [Product, setProduct] = useState({});
+  const [Product, setProduct] = useState({});
 
-    const [tryOn, setTryOn] = useState(false);
-    useEffect(() => {
-        axios
-            .get(`/api/product/products_by_id?id=${productId}`)
-            .then((response) => {
-                setProduct(response.data[0]);
-            })
-            .catch(err => alert(err))
-    }, []);
-    console.log(Product.color);
+  const [tryOn, setTryOn] = useState(false);
 
-    const bagHandler = () => {
-        // 필요한 정보를 cart field에 넣어준다.
-        dispatch(addToCart(productId));
-    };
-    const changeTry = () => {
-        setTryOn(!tryOn);
-    };
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <div style={{ width: "100%", padding: "3rem 4rem" }}>
-            <br />
-            <br />
 
-            <Row gutter={[30, 30]}>
-                <Col lg={12} sm={24}>
-                    {tryOn && <MakeUp color={Product.color} />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {tryOn && <br />}
-                    {!tryOn && <DetailProductImage detail={Product} />}
-                </Col>
-                <Col lg={12} sm={24}>
-                    <br />
-                    <br />
-                    <br />
-                    <h3>{Product.title}</h3>
-                    <hr />
-                    <p>{Product.price} 원</p>
-                    <p style={{ fontWeight: "bold" }}>선택 가능한 컬러</p>
-                    <Select style={{ width: "200px" }}>
-                        <option>제품이름 같은 다른 놈</option>
-                    </Select>
-                    <p> ^ 이곳에 제품이름 같은 놈들 링크 있으면 좋겠다</p>
-                    <br />
-                    <hr />
-                    <br />
-                    <Button size="large" shape="round">
-                        위시리스트
-                    </Button>
-                    <br />
-                    <br />
-                    <Button size="large" shape="round" onClick={bagHandler}>
-                        장바구니
-                    </Button>
-                    <br />
-                    <br />
-                    <Button size="large" shape="round" onClick={changeTry}>
-                        트라이온
-                    </Button>
-                </Col>
-            </Row>
-            <br />
-            <br />
-            <br />
-            <hr />
-            <p>상세 설명 이미지 | 근데 리뷰 기능 만들어 말어........</p>
-            <Image
-                src={`http://localhost:5000/${Product.description}`}
-                preview={false}
-            />
-        </div>
-    );
+  useEffect(() => {
+    axios
+      .get(`/api/product/products_by_id?id=${productId}`)
+      .then((response) => {
+        setProduct(response.data[0]);
+      })
+      .catch((err) => alert(err));
+  }, []);
+  console.log(Product.color);
+
+  const bagHandler = () => {
+    // 필요한 정보를 cart field에 넣어준다.
+    dispatch(addToCart(productId));
+  };
+  const changeTry = () => {
+    setTryOn(!tryOn);
+  };
+
+  
+
+  return (
+    <div className="detail-wrap">
+      <div className="detail-top-wrap">
+        <Row gutter={[30, 30]} className="detail-top-wrap-2">
+          <Col lg={12} sm={24}>
+            {tryOn && <MakeUp color={Product.color} />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {tryOn && <br />}
+            {!tryOn && <DetailProductImage detail={Product} />}
+          </Col>
+
+          <Col lg={12} sm={24} className="detail-info-wrap">
+            <div className="detail-title-wrap">
+              <h3>{Product.title}</h3>
+              {props.heart ? (
+                <AiFillHeart
+                  className="heart"
+                  onClick={() => {
+                    props.dispatch({ type: "heart-drain" });
+                  }}
+                />
+              ) : (
+                <AiOutlineHeart
+                  className="heart"
+                  onClick={() => {
+                    props.dispatch({ type: "heart-fill" });
+                  }}
+                />
+              )}
+            </div>
+
+            <p>{Product.price} 원</p>
+            <p style={{ fontWeight: "bold" }}>선택 가능한 컬러</p>
+            <Select style={{ width: "200px" }}>
+              <option>제품이름 같은 다른 놈</option>
+            </Select>
+
+            <button className="detail-btn" onClick={bagHandler}>
+              장바구니에 추가하기
+            </button>
+
+            <button className="detail-btn" onClick={changeTry}>
+              트라이온
+            </button>
+          </Col>
+        </Row>
+      </div>
+      <div className="detail-bottom-wrap"></div>
+      <p>상세 설명 이미지 | 근데 리뷰 기능 만들어 말어........</p>
+      <Image
+        src={`http://localhost:5000/${Product.description}`}
+        preview={false}
+      />
+    </div>
+  );
 }
 
 function stateprops(state) {
-    return {
-        navTG: state.reducer1,
-    };
+  return {
+    navTG: state.reducer1,
+    heart: state.reducer13,
+  };
 }
 
 export default connect(stateprops)(DetailProductPage);

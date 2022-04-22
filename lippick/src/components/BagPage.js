2a8513aa20bcf839514295799a5fe629d34f6df3
vjoe96 <87068212+vjoe96@users.noreply.github.com>
2022-04-22 18:37:21 +0900

    커밋

--- lippick/src/components/BagPage.js
+++ lippick/src/components/BagPage.js
@@ -1,79 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch} from "react-redux";
-import { getCartItems, removeCartItem } from "../_actions/user_actions"
-import UserCardBlock from './UserCardBlock';
-import Paypal from "./Paypal"
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getCartItems, removeCartItem } from "../_actions/user_actions";
+import UserCardBlock from "./UserCardBlock";
+import Paypal from "./Paypal";
 
 function BagPage(props) {
     const dispatch = useDispatch();
-    const [Total, setTotal] = useState(0)
-    const [CartNumber, setCartNumber] = useState(0)
+    const [Total, setTotal] = useState(0);
+    const [CartNumber, setCartNumber] = useState(0);
 
     useEffect(() => {
-
-        let cartItems = []
-
+        let cartItems = [];
 
         // 리덕스 User State안에 cart 안에 상품이 들어있는지 확인
-        if(props.user.userData && props.user.userData.cart) {
-            if(props.user.userData.cart.length > 0) {
-                props.user.userData.cart.forEach(item => {
-                    cartItems.push(item.id)
-                })
+        if (props.user.userData && props.user.userData.cart) {
+            if (props.user.userData.cart.length > 0) {
+                props.user.userData.cart.forEach((item) => {
+                    cartItems.push(item.id);
+                });
 
-                dispatch(getCartItems(cartItems, props.user.userData.cart ))
-                    .then(response => { 
-                        calculateTotal(response.payload)
-                    })
+                dispatch(
+                    getCartItems(cartItems, props.user.userData.cart)
+                ).then((response) => {
+                    calculateTotal(response.payload);
+                });
             }
         }
-        
-    }, [props.user.userData])
+    }, [props.user.userData]);
 
     let calculateTotal = (cartDetail) => {
         let total = 0;
 
-        cartDetail.map(item => {
-            total += parseInt(item.price,10) * item.quantity
-        })
-        setTotal(total)
-    }
+        cartDetail.map((item) => {
+            total += parseInt(item.price, 10) * item.quantity;
+        });
+        setTotal(total);
+    };
 
     let cartNum = (cartDetail) => {
         let total = 0;
 
-        cartDetail.map(item => {
-            total += parseInt(item.quantity,10)
-        })
-        setCartNumber(total)
-    } 
+        cartDetail.map((item) => {
+            total += parseInt(item.quantity, 10);
+        });
+        setCartNumber(total);
+    };
 
     let removeFromCart = (productId) => {
-        dispatch(removeCartItem(productId))
-        .then(response => {
-
-        })
-    }
-
-
+        dispatch(removeCartItem(productId)).then((response) => {});
+    };
 
     return (
-        <div style={{ width: '85%', margin: '3rem auto'}}>
+        <div style={{ width: "85%", margin: "3rem auto" }}>
             <h1>BagPage</h1>
             <div>
-            <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart}/>
+                <UserCardBlock
+                    products={props.user.cartDetail}
+                    removeItem={removeFromCart}
+                />
             </div>
 
-            <div style={{ marginTop: '3rem' }}>
+            <div style={{ marginTop: "3rem" }}>
                 <p>총 금액: {Total} 원</p>
             </div>
 
             <Paypal />
-
-
-        </div> 
-    )
+        </div>
+    );
 }
 
-
-export default BagPage
+export default BagPage;

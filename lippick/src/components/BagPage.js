dfd62d3d8f5e3e09dc7ae759be2044b0492d7f13
vjoe96 <87068212+vjoe96@users.noreply.github.com>
2022-04-23 22:46:45 +0900

    푸시

--- lippick/src/components/BagPage.js
+++ lippick/src/components/BagPage.js
@@ -1,18 +1,20 @@
-
-import React, { useEffect, useState } from 'react';
-import { useDispatch} from "react-redux";
-import { getCartItems, removeCartItem, onSuccessBuy } from "../_actions/user_actions"
-import UserCardBlock from './UserCardBlock';
-import Paypal from "./Paypal"
-import { Empty, Result } from 'antd';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+    getCartItems,
+    removeCartItem,
+    onSuccessBuy,
+} from "../_actions/user_actions";
+import UserCardBlock from "./UserCardBlock";
+import Paypal from "./Paypal";
+import { Empty, Result } from "antd";
 
 function BagPage(props) {
     const dispatch = useDispatch();
-    const [Total, setTotal] = useState(0)
-    const [CartNumber, setCartNumber] = useState(0)
-    const [ShowTotal, setShowTotal] = useState(false)
-    const [ShowSuccess, setShowSuccess] = useState(false)
-
+    const [Total, setTotal] = useState(0);
+    const [CartNumber, setCartNumber] = useState(0);
+    const [ShowTotal, setShowTotal] = useState(false);
+    const [ShowSuccess, setShowSuccess] = useState(false);
 
     useEffect(() => {
         let cartItems = [];
@@ -52,14 +54,7 @@ function BagPage(props) {
         setCartNumber(total);
     };
 
-    let removeFromCart = (productId) => {
-        // dispatch({ type: "bag-get" });
-        dispatch(removeCartItem(productId)).then((response) => {
-            if (response.payload.productInfo.length <= 0) {
-                setShowTotal(false);
-            }
-        })
-
+    let bagGet = () => {
         dispatch({ type: "bag-get" });
         setTimeout(() => {
             dispatch({ type: "bag-get" });
@@ -76,24 +71,32 @@ function BagPage(props) {
         setTimeout(() => {
             dispatch({ type: "bag-get" });
         }, 2000);
+    };
 
-    }
+    let removeFromCart = (productId) => {
+        // dispatch({ type: "bag-get" });
+        dispatch(removeCartItem(productId)).then((response) => {
+            if (response.payload.productInfo.length <= 0) {
+                setShowTotal(false);
+            }
+            bagGet();
+        });
+    };
 
     const transactionSuccess = (data) => {
-
-        dispatch(onSuccessBuy({
-            paymentData: data,
-            cartDetail: props.user.cartDetail
-        }))
-            .then(response => {
-                if(response.payload.success){
-                    setShowTotal(false)
-                    setShowSuccess(true)
-                }
+        dispatch(
+            onSuccessBuy({
+                paymentData: data,
+                cartDetail: props.user.cartDetail,
             })
-
-    }
-
+        ).then((response) => {
+            if (response.payload.success) {
+                setShowTotal(false);
+                setShowSuccess(true);
+            }
+        });
+        bagGet();
+    };
 
     return (
         <div style={{ width: "85%", margin: "3rem auto" }}>
@@ -105,33 +108,29 @@ function BagPage(props) {
                     onClick={() => {}}
                 />
             </div>
-         {ShowTotal ?
-                <div style={{ marginTop: '3rem' }}>
+            {ShowTotal ? (
+                <div style={{ marginTop: "3rem" }}>
                     <p>총 금액: {Total} 원</p>
                 </div>
-                : ShowSuccess ?
-                    <Result
-                    status="success"
-                    title="결제가 완료되었습니다."
+            ) : ShowSuccess ? (
+                <Result status="success" title="결제가 완료되었습니다." />
+            ) : (
+                <>
+                    <Empty
+                        description={false}
+                        image={Empty.PRESENTED_IMAGE_SIMPLE}
                     />
-                    :
-                    <> 
-                    <Empty description={false} image={Empty.PRESENTED_IMAGE_SIMPLE}/>
-                    </>
-            }
+                </>
+            )}
 
-            {ShowTotal && 
-                <Paypal  
-                    total={(Total/1243.5).toFixed(2)}
+            {ShowTotal && (
+                <Paypal
+                    total={(Total / 1243.5).toFixed(2)}
                     onSuccess={transactionSuccess}
                 />
-
-            }
-
-
-        </div> 
-    )
-
+            )}
+        </div>
+    );
 }
 
 export default BagPage;
